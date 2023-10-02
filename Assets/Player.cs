414c219d9baa17c66731f7a31b7e580d92f62d9a
Asad-Anjum <anjumasad967@gmail.com>
2023-10-02 00:25:38 -0400

    Small Fixes

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -2,15 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class Player : MonoBehaviour
 {
+    public GameObject canvas;
     public int health = 4;  
     public bool hasKey = false;
     public Animator anim;
     SpriteRenderer sr;
     private JumpLine jl;
     public Rotate rot;
+    public TMP_Text healthDisplay;
 
     void Start()
     {
@@ -34,7 +37,8 @@ public class Player : MonoBehaviour
         health -= damageAmount;
         anim.SetInteger("health", health);
         StartCoroutine(DamageFlash());
-        
+
+        StartCoroutine(HealthDisplay());
 
         
         if(!rot.turn && !rot.firstAfterTurn)
@@ -81,5 +85,15 @@ public class Player : MonoBehaviour
         sr.color = Color.white;
         yield return new WaitForSeconds(0.1f);
 
+    }
+
+    private IEnumerator HealthDisplay(){
+        TMP_Text display = Instantiate(healthDisplay, new Vector3(0f,0f,0f), Quaternion.identity);
+        display.transform.SetParent(canvas.transform, false);
+
+        display.text = health.ToString();
+
+        yield return new WaitForSeconds(2f);
+        Destroy(display);
     }
     }
